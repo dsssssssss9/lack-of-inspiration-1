8006e326fa7905b9bf715fd00e572f1c826ca132
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-02-27 21:53:13 +0000

    basic collision detection displayed on MicroBit

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@ KY040.OnPinPressed(DigitalPin.P14, function () {
         callimatrix.SetMatrixColorbright(0xffff00, index + 1, Pos, cbrightness.hp2)
         callimatrix.callimatrix_show()
         basic.pause(500)
+        if (Pos == Enemy && index == 7) {
+            Hit()
+        }
         callimatrix.SetMatrixColorbright(0x000000, index + 1, Pos, cbrightness.hp2)
         callimatrix.callimatrix_show()
     }
@@ -19,6 +22,11 @@ KY040.onTurned(direction.clockwise, function () {
     basic.pause(25)
     basic.clearScreen()
 })
+function Hit () {
+    basic.showIcon(IconNames.Angry)
+    basic.pause(1000)
+    basic.clearScreen()
+}
 KY040.onTurned(direction.counterclockwise, function () {
     basic.showArrow(ArrowNames.West)
     Pos += -1
@@ -28,6 +36,7 @@ KY040.onTurned(direction.counterclockwise, function () {
     basic.pause(25)
     basic.clearScreen()
 })
+let Enemy = 0
 let Pos = 0
 basic.showIcon(IconNames.Heart)
 KY040.setKY040(DigitalPin.P1, DigitalPin.P15)
@@ -36,8 +45,14 @@ callimatrix.callimatrix_del()
 Pos = 0
 callimatrix.SetMatrixColorbright(0x00ff00, 0, 0, cbrightness.hp25)
 callimatrix.callimatrix_show()
-let Enemy = randint(0, 7)
+Enemy = randint(0, 7)
 basic.pause(1000)
+basic.forever(function () {
+    callimatrix.SetMatrixColorbright(0x000000, 0, Pos + 1, cbrightness.hp6)
+    callimatrix.SetMatrixColorbright(0x000000, 0, Pos - 1, cbrightness.hp6)
+    callimatrix.SetMatrixColorbright(0x00ff00, 0, Pos, cbrightness.hp6)
+    callimatrix.callimatrix_show()
+})
 basic.forever(function () {
     callimatrix.SetMatrixColorbright(0x000000, 0, Pos + 1, cbrightness.hp6)
     callimatrix.SetMatrixColorbright(0x000000, 0, Pos - 1, cbrightness.hp6)
