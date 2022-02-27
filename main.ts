313c94c5cd863bd6a1ca3c693a869cc7bff1a5a6
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-02-27 20:28:35 +0000

    install 8x8 callimatrix & test

--- main.ts
+++ main.ts
@@ -15,6 +15,12 @@ KY040.onTurned(direction.counterclockwise, function () {
 })
 basic.showIcon(IconNames.Heart)
 KY040.setKY040(DigitalPin.P1, DigitalPin.P15)
+callimatrix.initNeoMatrix(DigitalPin.P2)
+callimatrix.callimatrix_del()
+callimatrix.SetMatrixColorbright(0x00ff00, 0, 0, cbrightness.hp25)
+callimatrix.SetMatrixColorbright(0xff0000, 3, 3, cbrightness.hp25)
+callimatrix.SetMatrixColorbright(0x0000ff, 7, 7, cbrightness.hp6)
+callimatrix.callimatrix_show()
 basic.forever(function () {
 	
 })
