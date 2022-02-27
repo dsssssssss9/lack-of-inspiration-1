f0b6b12a5278ed93c6f6f95b516528c471cd4ec6
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-02-27 20:17:53 +0000

    test connections

--- main.ts
+++ main.ts
@@ -1,14 +1,20 @@
 KY040.OnPinPressed(DigitalPin.P14, function () {
-    basic.showString("Hello!")
+    basic.showIcon(IconNames.No)
+    basic.pause(100)
+    basic.clearScreen()
 })
 KY040.onTurned(direction.clockwise, function () {
     basic.showArrow(ArrowNames.East)
+    basic.pause(25)
+    basic.clearScreen()
 })
 KY040.onTurned(direction.counterclockwise, function () {
     basic.showArrow(ArrowNames.West)
+    basic.pause(25)
+    basic.clearScreen()
 })
 basic.showIcon(IconNames.Heart)
-KY040.setKY040(DigitalPin.P15, DigitalPin.P1)
+KY040.setKY040(DigitalPin.P1, DigitalPin.P15)
 basic.forever(function () {
 	
 })
